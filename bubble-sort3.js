9660684d7a7af1b4191ed7cf59e1e68300703db1
jojayeon <jojayeon6152@gmail.com>
2024-08-27 00:39:16 +0900

    버블정렬 2번

---
+++ bubble-sort3.js
@@ -0,0 +1,27 @@
+function generateRandomArray(length, max) {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.floor(Math.random() * max) + 1);
+  }
+  return arr;
+}
+
+function bubblesort(arr) {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  }
+  return arr;
+}
+
+const RandomArray = generateRandomArray(10, 100);
+console.log(RandomArray);
+
+const sortArray = bubblesort(RandomArray);
+console.log(sortArray);
